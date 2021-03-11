e5c1ab7e5a05c64752ad571d9f9db9c8fd843132
timschott <tcs9pk@virginia.edu>
2021-03-11 14:49:51 -0500

    add app

---
+++ client/src/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Customers from './components/customers/customers';
+
+function App() {
+  return (
+    <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">React Express Starter</h1>
+        </header>
+        <Customers />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/index.js
@@ -0,0 +1,3 @@
+// exports components to support webpack alias.
+
+export { Customers } from 'customers/customers';
