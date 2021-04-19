f36ce76f4f08123800af0dc51a9a2dc38d129426
timschott <tcs9pk@virginia.edu>
2021-04-19 19:48:22 -0400

    initial connection

--- corpus/db-work/db.R
+++ corpus/db-work/db.R
@@ -1,2 +1,28 @@
 ### Load DDDB. ###
 
+library("mongolite")
+library("dotenv")
+
+## load creds from .env
+load_dot_env()
+
+url_path = Sys.getenv("MONGO_URI")
+
+## test, make sure connec is working 
+
+logomancing_db <- mongo(collection = "words", # table
+                  db = "fun-words", # db
+                  url = url_path, 
+                  verbose = TRUE)
+
+print(logomancing_db)
+
+logomances <- logomancing_db$find(query = '{}')
+
+## great
+
+## now, connect to new db and load data. 
+
+
+
+
