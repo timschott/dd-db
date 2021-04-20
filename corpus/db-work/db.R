e1e17068bc29df5756cfdfabdd9b960a79f10cb0
timschott <tcs9pk@virginia.edu>
2021-04-19 20:54:47 -0400

    add white noise and libra to db

--- corpus/db-work/db.R
+++ corpus/db-work/db.R
@@ -32,9 +32,9 @@ libra <- scan("../txts(cleaned)/Libra.txt",what="character",sep="\n")
 # book
 # dialogue
 
-libra_content <- libra
+## LIBRA
 
-# fill dialogue vec
+libra_content <- libra
 
 libra_dialogue_vec <- c(0)
 
@@ -60,5 +60,55 @@ colnames(libra_para_df) <- c("FakeID", "Content", "Dialogue", "Book")
 dd_db$insert(libra_para_df)
 # Complete! Processed total of 4743 rows.
 
+## WHITE NOISE 
 
+white_noise_content <- whitenoise
+
+white_noise_dialogue_vec <- c(0)
+
+for (i in 1:length(white_noise_content)) {
+  if (grepl('\"', white_noise_content[i]) == TRUE) {
+    white_noise_dialogue_vec[i] = 1
+  } else {
+    white_noise_dialogue_vec[i] = 0
+  }
+}
+
+white_noise_book <- rep("WhiteNoise", length(white_noise_content))
+white_noise_para_counter <- seq(1, length(white_noise_content))
+white_noise_fake_id <- paste0("WhiteNoise_",white_noise_para_counter)
+
+white_noise_para_matrix <- cbind(white_noise_fake_id, white_noise_content, white_noise_dialogue_vec, white_noise_book)
+
+white_noise_para_df <- as.data.frame(white_noise_para_matrix, stringsAsFactors = FALSE)
+colnames(white_noise_para_df) <- c("FakeID", "Content", "Dialogue", "Book")
+
+dd_db$insert(white_noise_para_df)
+# Complete! Processed total of 3283 rows.
+
+## AMERICANA 
+
+americana_content <- americana
+
+americana_dialogue_vec <- c(0)
+
+for (i in 1:length(americana_content)) {
+  if (grepl('“|”', americana_content[i]) == TRUE) {
+    americana_dialogue_vec[i] = 1
+  } else {
+    americana_dialogue_vec[i] = 0
+  }
+}
+
+americana_book <- rep("Americana", length(americana_content))
+americana_para_counter <- seq(1, length(americana_content))
+americana_fake_id <- paste0("Americana_",americana_para_counter)
+
+americana_para_matrix <- cbind(americana_fake_id, americana_content, americana_dialogue_vec, americana_book)
+
+americana_para_df <- as.data.frame(americana_para_matrix, stringsAsFactors = FALSE)
+colnames(americana_para_df) <- c("FakeID", "Content", "Dialogue", "Book")
+
+dd_db$insert(americana_para_df)
+# Complete! Processed total of 2602 rows.
 
