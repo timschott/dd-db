8d2920d28f6c783953aad0189bdac2f2bf7abccd
timschott <tcs9pk@virginia.edu>
2021-04-30 19:36:37 -0400

    initial database connection

--- server.js
+++ server.js
@@ -6,6 +6,32 @@ const port = 8000;
 // create new express app and save it as "app"
 const app = express()
 
+// db conf
+const DB_PORT = process.env.PORT || 8080
+
+// connect to database
+
+const MongoClient = require('mongodb').MongoClient
+
+var db
+
+// creds. grab from enviroment var file.
+require('dotenv').config({
+
+    path: '.env'
+})
+var url = process.env.MONGO_URI
+
+// connect to database.
+MongoClient.connect(url, {useUnifiedTopology: true}, (err, client) => {
+
+    if (err) return console.log(err)
+    db = client.db('dddb') // the database to this, try
+    app.listen(DB_PORT, () => {
+        console.log(`Server running at: http://localhost:${DB_PORT}/`);
+    })
+})
+
 // debug
 app.listen(port, () => {
   console.log(`App server now listening to port ${port}`);
